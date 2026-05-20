411519a2bc2888ebe5797ef6367bf7e7dd23f8d4
shashiv367 <vardhans367@gmail.com>
2026-05-20 22:52:30 +0530

    added

--- Variables.py
+++ Variables.py
@@ -65,3 +65,40 @@ print(type(g))
 
 x=10
 print(x==10)
+
+a,b,c = 10,20,30
+
+print(a,b,c)
+
+#swapping
+
+a=10
+b=20
+
+a,b=b,a
+print(a)
+print(b)
+
+x=100
+def test():
+    print(x)
+
+test()
+print(x)
+
+x=10
+def test():
+    y=30
+    print(y)
+
+test()
+print(x)
+
+x=122
+def test1():
+    y=25
+    print(y)
+def test2():
+    print(x)
+test1()
+test2()
